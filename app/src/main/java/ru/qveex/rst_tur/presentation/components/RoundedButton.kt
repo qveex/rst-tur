6bd1cc4b76f76b21d35b2a0e91c25de2b9999ec8
wolk2608 <wolk2608@yandex.ru>
2022-09-26 00:09:36 +0300

    Create button view and add new colors

---
+++ app/src/main/java/ru/qveex/rst_tur/presentation/components/RoundedButton.kt
@@ -0,0 +1,41 @@
+package ru.qveex.rst_tur.presentation.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.qveex.rst_tur.ui.theme.DarkButtonBackgroung
+import ru.qveex.rst_tur.ui.theme.DarkButtonContent
+import ru.qveex.rst_tur.ui.theme.LightButtonBackgroung
+import ru.qveex.rst_tur.ui.theme.LightButtonContent
+
+
+@Composable
+fun RoundedButton (
+    isDarkTheme: Boolean,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 14.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (isDarkTheme) DarkButtonBackgroung else LightButtonBackgroung,
+            contentColor = if (isDarkTheme) DarkButtonContent else LightButtonContent
+        ),
+        border = BorderStroke(1.dp, if (isDarkTheme) DarkButtonContent else LightButtonContent),
+        onClick = { onClick() }
+    ) {
+        Text(
+            text = text,
+            fontSize = MaterialTheme.typography.h6.fontSize
+        )
+    }
+}
