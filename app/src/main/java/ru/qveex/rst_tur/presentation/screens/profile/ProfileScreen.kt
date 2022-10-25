3dcb665c8763e50a4c4e38943aa5e2b153576991
qveex <npkololo@yandex.ru>
2022-10-26 02:40:45 +0300

    test UI in ProfileScreen

--- app/src/main/java/ru/qveex/rst_tur/presentation/screens/profile/ProfileScreen.kt
+++ app/src/main/java/ru/qveex/rst_tur/presentation/screens/profile/ProfileScreen.kt
@@ -3,14 +3,17 @@ package ru.qveex.rst_tur.presentation.screens.profile
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DarkMode
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.qveex.rst_tur.navigation.Screen
+import ru.qveex.rst_tur.presentation.components.CircleButton
 import ru.qveex.rst_tur.presentation.screens.main.SharedViewModel
 
 @Composable
@@ -25,6 +28,17 @@ fun ProfileScreen(
             .background(color = Color.DarkGray),
         contentAlignment = Alignment.Center,
     ) {
-        Text(text = "Profile")
+        CircleButton(
+            text = "click",
+            onClicked = { },
+            gradient = Brush.linearGradient(
+                listOf(
+                    Color(0xffc31432),
+                    Color(0xff240b36),
+                )
+            ),
+            textColor = Color.White,
+            image = Icons.Outlined.DarkMode
+        )
     }
 }
